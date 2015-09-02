f781656967fb43fd579893b60d9e00f3f88d9dba
George <7jagjag@gmail.com>
2015-09-02 00:22:54 -0600

    change format

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ while [[ true ]]; do
 	DATE=`date +%Y-%m-%d`
 	CAMBIO="`node server`";
 	echo -e "`date` | Q$CAMBIO = \$1.00";
-	echo "$DATE - $CAMBIO" >> exchange.txt
+	echo "$DATE = $CAMBIO" >> exchange.txt
 	sleep 1d
 done
 
